b5cc191c362d37ffa59543b3e26cd0657dafe49e
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-10-26 18:11:56 -0500

    get request

--- flappy-bird/Assets/AddFriend.cs
+++ flappy-bird/Assets/AddFriend.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 using TMPro;
 using Firebase.Database;
 using Firebase.Extensions;
+using Google.MiniJSON;
+using System;
 
 public class AddFriend : MonoBehaviour
 {
     public TMP_Text username;
     public GameObject requestPanel;
     private User selectedUser;
-   public void ShowRequestInfo(Id id)
+    private DatabaseReference mDatabase;
+
+
+
+    private void OnEnable()
+    {
+        Lobby.OnLobbyEnter += InitRequestController;
+
+    }
+    private void OnDisable()
+    {
+        Lobby.OnLobbyEnter -= InitRequestController;
+
+    }
+
+    public void InitRequestController()
+    {
+
+
+        string myId = PlayerPrefs.GetString("userID");
+
+        var userOnlineRef = FirebaseDatabase.DefaultInstance
+        .GetReference($"users/{myId}");
+
+        userOnlineRef.ChildChanged += HandleRequests;
+
+    }
+
+    private void HandleRequests(object sender, ChildChangedEventArgs args)
+    {
+
+        if (args.DatabaseError != null)
+        {
+            Debug.Log(args.DatabaseError.Message);
+            return;
+        }
+        Debug.Log(args.Snapshot.Value);
+    }
+
+    public void ShowRequestInfo(Id id)
     {
         selectedUser = UsersOnlineController.instance.activeUsers[id.userId];
         requestPanel.SetActive(true);
@@ -20,8 +61,34 @@ public class AddFriend : MonoBehaviour
     public void SendFriendRequest()
     {
 
+        mDatabase = FirebaseDatabase.DefaultInstance.RootReference;
 
-       
+        string myId = PlayerPrefs.GetString("userID");
+        string requestId = myId + "r";
+
+        FriendRequest friendRequest = new FriendRequest(myId, false, requestId);
+        Debug.Log("Send"+ myId);
+
+        string json = JsonUtility.ToJson(friendRequest);
+        mDatabase.Child("users").Child(selectedUser.id).Child("request").Child(requestId).SetRawJsonValueAsync(json);
+
+
 
      }
+
+
+   
+    [System.Serializable]
+    public class FriendRequest
+    {
+        public string sender;
+        public bool accepted;
+        public string requestId;
+        public FriendRequest(string sender, bool accepted, string requestId)
+        {
+            this.sender = sender;
+            this.accepted = accepted;
+            this.requestId = requestId;
+        }
+    }
 }
