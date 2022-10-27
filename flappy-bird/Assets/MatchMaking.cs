67eb35520df114ddf7c727e5cc20593b6ced4ccb
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2022-10-27 09:31:00 -0500

    matchmaking

--- flappy-bird/Assets/MatchMaking.cs
+++ flappy-bird/Assets/MatchMaking.cs
@@ -5,18 +5,22 @@ using TMPro;
 using Firebase.Database;
 using Firebase.Auth;
 using Firebase.Extensions;
+using System;
+using UnityEngine.SceneManagement;
 
 public class MatchMaking : MonoBehaviour
 {
     DatabaseReference mDatabase;
    public TMP_Text playerList;
    public TMP_Text playerCount;
+   public TMP_Text fullMatch;
     public int minPlayers;
+
 
    public void SearchMatch()
     {
         mDatabase = FirebaseDatabase.DefaultInstance.RootReference;
-
+        bool matchFound = false;
         FirebaseDatabase.DefaultInstance
         .GetReference("partidas")
         .GetValueAsync().ContinueWithOnMainThread(task =>
@@ -43,10 +47,21 @@ public class MatchMaking : MonoBehaviour
                         Dictionary<string, object> players = (Dictionary<string, object>)partida["players"];
                         int playerIndex = players.Keys.Count + 1;
                         mDatabase.Child("partidas").Child(item.Key).Child("players").Child("player" + playerIndex).SetValueAsync(username);
+                        matchFound = true;
+                        HandleMatch(item.Key);
+
+                        if (playerIndex == minPlayers)
+                        {
+                            mDatabase.Child("partidas").Child(item.Key).Child("full").SetValueAsync(true);
+
+                        }
                     }
                 }
 
-
+                if (!matchFound)
+                {
+                    CreateMatch();
+                }
             }
            
             
@@ -63,12 +78,70 @@ public class MatchMaking : MonoBehaviour
         string username = PlayerPrefs.GetString("username");
         mDatabase = FirebaseDatabase.DefaultInstance.RootReference;
         mDatabase.Child("partidas").Child(userId).Child("players").Child("player1").SetValueAsync(username);
-        mDatabase.Child("partidas").Child(userId).Child("full").SetValueAsync(false);
+        mDatabase.Child("partidas").Child(userId).Child("full").SetValueAsync(false).ContinueWithOnMainThread(task =>
+        {
+
+            HandleMatch(userId);
+        });
+
+       
+    }
+
+    public void HandleMatch(string matchId)
+    {
+        var matchRef = FirebaseDatabase.DefaultInstance
+        .GetReference($"partidas/{matchId}");
+
+        matchRef.ValueChanged += UpdateMatch;
+
+    }
+
+    private void UpdateMatch(object sender, ValueChangedEventArgs args)
+    {
+        playerList.text = "";
+        if (args.DatabaseError != null)
+        {
+            return;
+        }
+
+        Dictionary<string, object> partida = (Dictionary<string, object>)args.Snapshot.Value;
+        if (partida.ContainsKey("players"))
+        {
+            Dictionary<string, object> players = (Dictionary<string, object>)partida["players"];
+
+            playerCount.text = players.Keys.Count + "/" + minPlayers + "jugadores";
+
+            for (int i = 0; i < players.Keys.Count; i++)
+            {
+                int key =i+1;
+                playerList.text += players["player"+key.ToString()]+ "\n";
+            }
+            
+        }
+
+        if (partida.ContainsKey("fulll"))
+        {
+            bool isFull = (bool)partida["full"];
+            if (isFull)
+            {
+                StartCoroutine(ChangeScene());
+            }
+        }
     }
 
 
+    IEnumerator ChangeScene()
+    {
+        fullMatch.gameObject.SetActive(true);
+        int time = 10;
+        while (time >= 0)
+        {
+            fullMatch.text = "Empezando partida en" + time;
+            yield return new WaitForSeconds(1);
+            time -= 1;
+        }
 
+        SceneManager.LoadScene("main");
 
-
-
+    }
 }
